b5c374465ebd2a182b90720b99809f4c1b48b178
Tyler Long <tyler4long@gmail.com>
2017-03-02 23:18:29 +0800

    webpack external

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const nodeExternals = require('webpack-node-externals')
+
 const config = {
   target: 'node',
+  externals: [nodeExternals()],
   entry: {
     'src/index': './src/index.js',
     'test/index': './test/index.js'
